6b534a43073bb12d95f0025ee992d98c275e3fc2
Fatma072 <ftmx.na@gmail.com>
2025-07-19 13:57:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,2 @@
-import streamlit as st
+streamlit run streamlit_app.py
 
-st.set_page_config(page_title="Simulator Kimia", layout="wide")
-
-st.title("💡 Aplikasi Simulator Instrumen Kimia")
-
-st.markdown(
-    """
-    ## Selamat Datang 👋
-
-    Aplikasi ini dirancang untuk membantu Anda memahami berbagai **instrumen laboratorium kimia**, 
-    serta menyediakan panduan penting mengenai **penanganan bahan kimia** dan **keselamatan kerja (K3)**.
-
-    Silakan gunakan menu di sebelah kiri untuk menjelajahi simulasi dan informasi yang tersedia.
-    """
-)
