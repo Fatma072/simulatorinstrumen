fb2e926f9ec6facfd1ed5198527bde200585d558
Fatma072 <ftmx.na@gmail.com>
2025-07-19 13:54:33 +0700

    Create Spektrofotometer.py

---
+++ Spektrofotometer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("🔬 Simulator Spektrofotometer UV-Vis")
+st.write("""
+Simulasi ini membantu Anda memahami prinsip kerja spektrofotometer:
+
+- Pilih panjang gelombang
+- Visualisasi kurva absorbansi
+- Penentuan konsentrasi berdasarkan hukum Beer-Lambert
+""")
